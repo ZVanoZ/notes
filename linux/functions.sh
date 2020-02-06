0cba503dee39b0f22c8cd9ce3848dd3ed73b5dbf
ZVanoZ <30519367+ZVanoZ@users.noreply.github.com>
2020-02-06 19:20:43 +0200

    Create functions.sh

---
+++ linux/functions.sh
@@ -0,0 +1,20 @@
+# Создает символическую ссылку, если она еще не существует
+# Аргумент1: источник. Т.е. файл, на который будем ссылаться
+# Аргумент2: назначение. Путь к файлу-ссылке, который создаем.
+function makeLink() {
+  echo '-- makeLink'
+  if [[ $# -ne 2]]; then
+    echo "fail: invalid arguments count"
+  else
+    inpPath=$1
+    outPath=$2
+    isForce='FALSE'
+    if [ -e $outPath -o -h $outPath ]; then
+      echo "skip: "$outPath
+    else
+      ln -s $inpPath $outPath
+      echo "create link: "$outPath
+    fi
+  fi
+}
+makeLink '15-xdebug.ini' '/opt/remi/php56/root/etc/php.d/15-xdebug.ini'
