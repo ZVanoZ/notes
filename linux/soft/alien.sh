a8a40ca24f0e82e1725e62186cb67a981d6b13ec
ZVanoZ <30519367+ZVanoZ@users.noreply.github.com>
2020-10-26 10:02:04 +0200

    Update alien.sh

--- linux/soft/alien.sh
+++ linux/soft/alien.sh
@@ -10,7 +10,7 @@
 
 # Конвертируем RPM пакет клиента Oracle в DEB для установки в Debian-базированных Linux
 $ cd /mnt/zim-1tb/data/install/Oracle Client/11.2.0.4.0/linux
-$ sudo alien --to-deb --scripts oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm 
-oracle-instantclient11.2-devel_11.2.0.4.0-2_amd64.deb generated
+$ sudo alien --to-deb --scripts oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm
+oracle-instantclient11.2-basic_11.2.0.4.0-2_amd64.deb generated
 
 #-------------------------------------------------------------------------------------------
