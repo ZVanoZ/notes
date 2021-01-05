ab53e39a94a0297c2355b28d897b32a3bc3b58c1
ZVanoZ <30519367+ZVanoZ@users.noreply.github.com>
2021-01-05 15:59:01 +0200

    Update docker-phpunit.sh

--- docker/docker-phpunit.sh
+++ docker/docker-phpunit.sh
@@ -15,9 +15,31 @@ cd ..  # На папку выше, чтобы "phpunit.xml" был в текущ
 # Запуск при помощи сборок, которые содержат php-cli
 #-----
 
-# "phpunit" + "php 5.3"
+# -- Работает
+# REPOSITORY                                          TAG                  IMAGE ID            CREATED             SIZE
+# tomsowerby/php-5.3                                  cli                  892f891e5196        5 years ago         440MB
+#
+# PHPUnit 4.8.36 by Sebastian Bergmann and contributors.
+# Runtime:	PHP 5.3.29
 docker run -v $(pwd):/app --workdir="/app" --rm tomsowerby/php-5.3:cli vendor/bin/phpunit --verbose
 
+# -- Работает
+# REPOSITORY                                          TAG                  IMAGE ID            CREATED             SIZE
+# php                                                 5.3-cli              7f2a82c91480        6 years ago         729MB
+#
+# PHPUnit 4.8.36 by Sebastian Bergmann and contributors.
+# Runtime:	PHP 5.3.29
+# Configuration:	/app/phpunit.xml
+docker run -v $(pwd):/app --workdir="/app" --rm php:5.3-cli vendor/bin/phpunit --verbose
+
+#  -- Работает
+# REPOSITORY                                          TAG                  IMAGE ID            CREATED             SIZE
+# seti/php53                                          latest               70b21ba63d9c        7 months ago        298MB
+#
+# PHPUnit 4.8.36 by Sebastian Bergmann and contributors.
+# Runtime:	PHP 5.3.29-Ubuntu/14.04-SergeyD/14.4
+# Configuration:	/app/phpunit.xml
+docker run -v $(pwd):/app --workdir="/app" --rm seti/php53 /app/vendor/bin/phpunit --verbose
 #------------------------------------------------------------------------------
 # Запуск специализированного образа
 # @TODO: разобраться как запускать "phpunit" в среде "docker"
