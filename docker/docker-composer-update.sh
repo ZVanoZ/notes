ddcf632ee5ab0918d8fd0e23bd51cf92e7520d78
Ivan Zahlyada <30519367+ZVanoZ@users.noreply.github.com>
2021-10-01 15:38:58 +0300

    Update docker-composer-update.sh

--- docker/docker-composer-update.sh
+++ docker/docker-composer-update.sh
@@ -1,29 +1,7 @@
 #!/usr/bin/env bash
-cd $(dirname $(readlink -e $0)) #переход в директорию текущего скрипта
 #------------------------------------------------------------------------------
-#docker run --rm -v $(pwd):/app  tomsowerby/php-5.3-composer -vvv install
-
-#docker run --rm --interactive --tty \
-# --volume $PWD:/app \
-# composer:2.0 \
-# composer install
-
-docker run --rm --interactive --tty \
---volume $PWD:/app \
-composer:2.0 \
-composer update
+#переход в директорию текущего скрипта
+cd $(dirname $(readlink -e $0)) 
 #------------------------------------------------------------------------------
-# Если composer установлен внутрь контейнера, то можно так
-#-----
-# Перегенерировать autoload
-# Выполняем, когда  в "composer.json" (autoload/psr-4):
-# > 1. добавляем модуль;
-# > 2. удаляем модуль
-
-# Вариант 1. Раздельная команда
-# docker container exec laminas-mvc-skeleton composer -v dump-autoload
-# docker container exec laminas-mvc-skeleton chmod -vR 777 ./vendor/
-
-# Вариант 2. Одной командой
-docker container exec laminas-mvc-skeleton bash -c 'composer -vvv dump-autoload && chmod -vR 777 ./vendor/'
+...
 #------------------------------------------------------------------------------
